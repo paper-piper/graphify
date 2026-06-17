cf5d9e2d265b70814900ead9a6e1308cabc80613
Yoni Reichert <yonatanreichert@gmail.com>
2026-06-17 14:23:38 +0300

    using db/index.ts to load database into comfterable AdjacencyList for quering

---
+++ services/graph/buildAdjacencyList.ts
@@ -0,0 +1,18 @@
+import { AdjacencyList } from "./graph.types";
+import { get_nodes_and_edges } from "../../db/get_nodes_and_edges";
+
+export async function buildAdjacencyList(): Promise<AdjacencyList>
+{
+  const adj: AdjacencyList = new Map();
+  const [nodes, edges] = await get_nodes_and_edges();
+
+  nodes.forEach(node => {
+    adj.set(node.id, new Set());
+  });
+  edges.forEach(edge => {
+    adj.get(edge.source_node)?.add(edge.target_node);
+    adj.get(edge.target_node)?.add(edge.source_node);
+  });
+
+  return adj;
+}
